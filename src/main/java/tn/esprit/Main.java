9cb7c5985f81ce60515e62bf5028f74b9481b0f5
amal <amal.hkiri@esprit.tn>
2024-02-15 17:00:34 +0000

    v1

--- src/main/java/tn/esprit/Main.java
+++ src/main/java/tn/esprit/Main.java
@@ -8,7 +8,7 @@ import edu.esprit.utils.DataSource;
 
 import java.util.Set;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+/// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
